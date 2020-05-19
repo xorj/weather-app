35ef065d98d8c3b5d913d67387da27f6028fb54f
xorj <jorge.santos@dcomp.ufs.br>
2020-05-19 20:49:16 -0300

    Updates

---
+++ precache-manifest.a4652cf5e943377042a9c5c0c6a79441.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418cfc466508bccdb0a8cd4761b8d2de",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "c97a5bcc7e612725708a",
+    "url": "/weather-app/static/css/main.4ad03d4d.chunk.css"
+  },
+  {
+    "revision": "33ce13720e0aae5440a8",
+    "url": "/weather-app/static/js/2.d8f3fcc0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-app/static/js/2.d8f3fcc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c97a5bcc7e612725708a",
+    "url": "/weather-app/static/js/main.3cf70e75.chunk.js"
+  },
+  {
+    "revision": "b42630db07c8dc9c26e3",
+    "url": "/weather-app/static/js/runtime-main.c6663dd0.js"
+  }
+]);
