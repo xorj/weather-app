340c5fd151ee27ce8c8ba2283e75063cd9346b33
xorj <jorge.santos@dcomp.ufs.br>
2020-05-19 20:40:15 -0300

    adding build files

---
+++ build/precache-manifest.a027112b653d34fd471d52390ea72e85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc1bcb05aaa600891957e499fb6ac3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c97a5bcc7e612725708a",
+    "url": "/static/css/main.4ad03d4d.chunk.css"
+  },
+  {
+    "revision": "2cbbd78c1aab4bd2a0be",
+    "url": "/static/js/2.32327c47.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.32327c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c97a5bcc7e612725708a",
+    "url": "/static/js/main.3cf70e75.chunk.js"
+  },
+  {
+    "revision": "72c8e7bec8507435f27b",
+    "url": "/static/js/runtime-main.1f43575b.js"
+  }
+]);
